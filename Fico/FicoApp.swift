fa124eee2ce854143ebdc3175b84ec223ccd2602
Gabriel Tanod <gabrielctanod@gmail.com>
2025-10-13 15:16:19 +0700

    Initial Commit

---
+++ Fico/FicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FicoApp.swift
+//  Fico
+//
+//  Created by Gabriel Tanod on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FicoTests/FicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FicoTests.swift
+//  FicoTests
+//
+//  Created by Gabriel Tanod on 13/10/25.
+//
+
+import Testing
+@testable import Fico
+
+struct FicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
